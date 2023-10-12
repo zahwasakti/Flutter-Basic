6cd056c21338efadefc95bce71b3e79407316d2b
zahwasakti <zahwaaazahirah@gmail.com>
2023-10-12 20:52:21 +0700

    widget3

--- flutter_project_1/lib/widget/widget3.dart
+++ flutter_project_1/lib/widget/widget3.dart
@@ -8,11 +8,37 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+
+final _myText = TextEditingController();
+String Status = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar
       (title: const Text("Text Field Widget")),
+      body: Center(child: Column(children: [
+        Container(
+          child: Text(Status),),
+        TextField(
+          controller: _myText,
+          decoration: InputDecoration(
+            hintText: "Ubah Status",
+            border: OutlineInputBorder(),
+            suffixIcon: IconButton(onPressed: (){
+              _myText.clear();
+            }, icon: Icon(Icons.clear))
+          ),
+        ),
+        MaterialButton(
+          onPressed: (){
+            setState(() {
+              Status = _myText.text;
+            });
+          }, color: Colors.blue, child: Text("Update Status", style: TextStyle(color: Colors.white),),)
+      ], 
+      ),
+      ),
     );
   }
 }
