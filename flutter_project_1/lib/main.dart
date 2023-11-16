68da76410c59f4df9bfe49f402083ba4c75dd8e8
zahwasakti <zahwaaazahirah@gmail.com>
2023-11-16 20:56:51 +0700

    update fitur

--- flutter_project_1/lib/main.dart
+++ flutter_project_1/lib/main.dart
@@ -1,31 +1,19 @@
-import 'package:flutter/material.dart';
-//import 'package:flutter_project_1/widget/widget1.dart';
-// import 'package:flutter_project_1/widget/widget2.dart';
-import 'package:flutter_project_1/widget/widget3.dart';
+import 'package:flutter/material.dart'; 
+import 'ui/beranda.dart'; 
 
-//Flutter Project Zahwa
+void main() { 
+runApp(const MyApp()); 
+} 
 
-void main() {
-  runApp(const MyApp());
-}
+class MyApp extends StatelessWidget { 
+const MyApp({super.key}); 
 
-// main page, untuk mengatur tittle dan upper bar
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, //menghilangkan banner demo
-      title: 'Aplikasi 1',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellowAccent),
-        useMaterial3: false,
-      ),
-      home: MyWidget(),
-    );
-  }
-}
-
-// isi aplikasi
+@override 
+Widget build(BuildContext context) { 
+return const MaterialApp( 
+debugShowCheckedModeBanner: false, 
+title: 'Klinik', 
+home: Beranda(), 
+); 
+} 
+} 
---
+++ flutter_project_1/lib/ui/beranda.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart'; 
+
+import '../widget/sidebar.dart'; 
+
+class Beranda extends StatelessWidget { 
+const Beranda({super.key}); 
+
+@override 
+Widget build(BuildContext context) { 
+return Scaffold( 
+drawer: Sidebar(), 
+appBar: AppBar(title: Text("beranda")), 
+body: Center( 
+child: Text("Selamat Datang"), 
+), 
+); 
+} 
+} 
